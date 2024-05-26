a2b05929630ea96baf9fc6f8fb2a5c81e0dbd4ca
naman995 <rajputnaman823@gmail.com>
2024-05-27 00:56:26 +0530

    custom pipe

--- angu/src/app/app.module.ts
+++ angu/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostComponent } from './post/post.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { AppendPipe } from './Pipes/append.pipe';
+import { AppendCLIPipe } from './Pipes/append-cli.pipe';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { PostListComponent } from './post-list/post-list.component';
     NavbarComponent,
     PostComponent,
     PostListComponent,
+    AppendPipe,
+    AppendCLIPipe,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
--- angu/src/app/post/post.component.ts
+++ angu/src/app/post/post.component.ts
@@ -6,12 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css'],
 })
 export class PostComponent {
-    postArray:Array<string>=[
-    'Post 1',
-    'Post 2',
-    'Post 3',
-    'Post 4',
-    'Post 5',
-    'Post 6'
-    ]
+  userDetails = {
+    name: 'John Doe',
+    city: 'New York',
+    countryCode: 'US',
+  };
 }
