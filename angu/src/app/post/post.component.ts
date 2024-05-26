3df5ba7e7807a8056c45ac40a7393422fde64f56
naman995 <rajputnaman823@gmail.com>
2024-05-26 21:11:40 +0530

    Date Pipe

--- angu/src/app/post/post.component.ts
+++ angu/src/app/post/post.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css'],
 })
 export class PostComponent {
-  stepForm!: string;
-  onClick(status: string) {
-    this.stepForm = status;
-  }
+  title:string = 'Post Title';
+  count:number = 10000000;
+  deval:number = 3.1245432;
+  curr: number = 123456;
+  date: Date = new Date();
 }
