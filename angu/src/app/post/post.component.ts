3ec83cae2236cf142a72be2f289c205627599521
naman995 <rajputnaman823@gmail.com>
2024-05-28 20:58:55 +0530

    form

--- angu/src/app/post/post.component.ts
+++ angu/src/app/post/post.component.ts
@@ -1,24 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { PostService } from '../Services/post.service';
-import { Post } from '../models/post';
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent implements OnInit {
-  posts!: Array<any>;
-  constructor(private postService: PostService) {
-    // let postService = new PostService();
-    this.posts = postService.postList;
-  }
-  addNewData() {
-    let newPost:Post = {
-      id: 7,
-      postTitle: 'Post 7',
-    };
-    this.postService.addPost(newPost);
-  }
-  ngOnInit(): void {}
+export class PostComponent {
+  constructor() {}
+  onSubmit = (data:NgForm) => {
+    console.log(data);
+  };
 }
