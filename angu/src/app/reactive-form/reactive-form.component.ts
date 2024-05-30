882df284e356aaa9e7e776d015634f83feb4b5f0
naman995 <rajputnaman823@gmail.com>
2024-05-30 05:33:11 +0530

    Creating Reactive Forms Control Programmatically

--- angu/src/app/reactive-form/reactive-form.component.ts
+++ angu/src/app/reactive-form/reactive-form.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, NgForm } from '@angular/forms';
+import { FormControl, NgForm, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css'],
-
 })
 export class ReactiveFormComponent implements OnInit {
-  constructor() {}
+  form: any;
+  constructor() {
+    this.form = new FormGroup({
+      fullName: new FormControl(),
+      email: new FormControl(),
+      address: new FormControl(),
+    });
+  }
 
   ngOnInit(): void {}
-  onSubmit = (f: NgForm) => {
-    console.log(f.value);
-  };
-  getValue = (f: FormControl) => {
-    console.log(f);
-  };
 }
