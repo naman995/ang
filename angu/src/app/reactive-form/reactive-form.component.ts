ae62895d4364770492f494618e48951f7d45180b
naman995 <rajputnaman823@gmail.com>
2024-05-30 10:07:03 +0530

    More validations

--- angu/src/app/reactive-form/reactive-form.component.ts
+++ angu/src/app/reactive-form/reactive-form.component.ts
@@ -8,12 +8,30 @@ import { FormControl, NgForm, FormGroup, Validators } from '@angular/forms';
 })
 export class ReactiveFormComponent implements OnInit {
   form: any;
+  // emailRjx: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$';
   constructor() {
     this.form = new FormGroup({
-      fullName: new FormControl('',[Validators.required, Validators.minLength(5)]),
-      email: new FormControl(),
-      address: new FormControl(),
+      fullName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        //  Validators.pattern(this.emailRjx)
+      ]),
+      address: new FormControl('', Validators.required),
     });
+  }
+  get fullName() {
+    return this.form.get('fullName');
+  }
+  get email() {
+    return this.form.get('email');
+  }
+
+  get address() {
+    return this.form.get('address');
   }
 
   ngOnInit(): void {}
