ef88cba0b76972bb62ccddce7246b095415f6d87
naman995 <rajputnaman823@gmail.com>
2024-06-04 11:07:50 +0530

    Rxjs Observable vs Function

--- angu/src/app/app.component.ts
+++ angu/src/app/app.component.ts
@@ -1,20 +1,24 @@
-import { Component ,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'angu';
 
   ngOnInit(): void {
-      const obsTest$ = new Observable(observer =>{
-        console.log("observable starts");
-      }).subscribe();
-  }
+    const obsTest$ = new Observable((observer) => {
+      observer.next('Return from observable');
+    }).subscribe((value) => console.log(value));
 
+    const obsTest = function () {
+      return 'return from function';
+    };
+    const returnData = obsTest();
+    console.log(returnData);
+  }
 }
 // to call onservable we need to subscribe it
